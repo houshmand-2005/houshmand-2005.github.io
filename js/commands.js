88c3ac66530c46ebccb3c00dce2914430be5e407
Houshmand <77535700+houshmand-2005@users.noreply.github.com>
2022-08-18 12:16:50 +0430

    add new version

---
+++ js/commands.js
@@ -0,0 +1,54 @@
+var github = "https://github.com/houshmand-2005/";
+var email = 'mailto:houshmand2005@gmail.com';
+
+whois = [
+  "<br>",
+  "Hey👋",
+  "I’m Amir Mohammad Houshmand",
+  "I’m 17 and My favorite technologyes are : ",
+  "Python, Django, Docker, Django REST framework (DRF), fast api and react",
+  "<br>"
+];
+
+
+
+social = [
+  "<br>",
+  "You can find me on:",
+  'GitHub         <a href="' + github + '" target="_blank">github/houshmand-2005' + "</a>",
+  'Telegram       <a href="https://t.me/Houshmand_am" target="_blank">@Houshmand_am</a>',
+  "<br>"
+];
+
+
+
+projects = [
+  "<br>",
+  '<p>You can use my Django booklet from <a href="https://github.com/houshmand-2005/hash_neco">here</a>', 
+  'Also I have a django project you can see website from here <a href="https://learning-room.iran.liara.run/"><code>https://learning-room.iran.liara.run/</code></a>', 
+  'and you can find the source of this projcet from <a href="https://github.com/houshmand-2005/django4">here</a><br>',
+  'I have a encoder decoder text base by django you can see source from <a href="https://github.com/houshmand-2005/RmzHash/tree/RmzHash">here</a> and the live verion : <a href="https://rmzhash.iran.liara.run/"><code>https://rmzhash.iran.liara.run/</code></a></p>',
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          About Me',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Email me',
+  '<span class="command">clear</span>          Clear terminal',
+  "<br>",
+];
+banner = [
+  '██╗░░██╗░█████╗░██╗░░░██╗░██████╗██╗░░██╗███╗░░░███╗░█████╗░███╗░░██╗██████╗░',
+  '██║░░██║██╔══██╗██║░░░██║██╔════╝██║░░██║████╗░████║██╔══██╗████╗░██║██╔══██╗',
+  '███████║██║░░██║██║░░░██║╚█████╗░███████║██╔████╔██║███████║██╔██╗██║██║░░██║',
+  '██╔══██║██║░░██║██║░░░██║░╚═══██╗██╔══██║██║╚██╔╝██║██╔══██║██║╚████║██║░░██║',
+  '██║░░██║╚█████╔╝╚██████╔╝██████╔╝██║░░██║██║░╚═╝░██║██║░░██║██║░╚███║██████╔╝',
+  '╚═╝░░╚═╝░╚════╝░░╚═════╝░╚═════╝░╚═╝░░╚═╝╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═════╝░',
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
